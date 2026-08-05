64ae67f0dc62956f7dbfd9a358e111dabfc2259b
Vikash Singh <vikas040805gmail@Vikashs-MacBook-Air.local>
2026-08-05 16:44:23 +0530

    music_queue

---
+++ phase_01_html_css_js/day_11/homework/localstorage.js
@@ -0,0 +1,5 @@
+// Local Storage
+// A feature of webrowser which allows you to 
+// store data permanently in ur system
+
+// to share data in your local storage:
--- phase_01_html_css_js/day_11/homework/script.js
+++ phase_01_html_css_js/day_11/homework/script.js
@@ -9,13 +9,28 @@ form.addEventListener("submit", function(event){
         age: document.getElementById("age").value,
         course: document.getElementById("course").value
     };
+    localStorage.setItem("students",JSON.stringify(student))
 
+    
+
+    form.reset();
+    renderData()
+
+});
+let renderData=()=>{
+    //getting data from local storage
+    let student=localStorage.getItem("students")
+    console.log("data from local storage",student)
+    let student_obj=JSON.parse(student)
+    console.log(typeof(student_obj))
     card.innerHTML = `
         <div class="profile-card">
             <h2>Student Profile</h2>
-            <p><strong>Name:</strong> ${student.name}</p>
-            <p><strong>Age:</strong> ${student.age}</p>
-            <p><strong>Course:</strong> ${student.course}</p>
+            <p><strong>Name:</strong> ${student_obj.name}</p>
+            <p><strong>Age:</strong> ${student_obj.age}</p>
+            <p><strong>Course:</strong> ${student_obj.course}</p>
         </div>
-    `
-});
+    `;
+
+}
+renderData()
