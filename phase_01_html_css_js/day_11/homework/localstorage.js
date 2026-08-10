649d7934412b95406d41292888c555ed4bdf904c
Vikash Singh <vikas040805@gmail.com>
2026-08-10 20:28:07 +0530

    few changes in day_11 homework

--- phase_01_html_css_js/day_11/homework/localstorage.js
+++ phase_01_html_css_js/day_11/homework/localstorage.js
@@ -1,5 +1,11 @@
-// Local Storage
-// A feature of webrowser which allows you to 
-// store data permanently in ur system
 
-// to share data in your local storage:
+// Local Storage
+// It is an feature of web browser which allows you to 
+// store data permanently in your system
+
+// to store data in local storage
+localStorage.setItem("users","Aman") 
+// {"key":data} // {"users":"Aman"}
+
+// to retrive data
+localStorage.getItem("users")
